d7019d86913300eee451499ff2e125dc13234917
Lars Kruse <lakruzz@inc-inc.dk>
2024-12-29 13:33:45 +0000

    added testfile

--- tests/test_gh-semver-no-subcmd.py
+++ tests/test_gh-semver-no-subcmd.py
@@ -2,7 +2,7 @@ import subprocess
 import unittest
 import pytest
 import os
-
+from .testbed import Testbed
 
 class TestGhSemverNoSubcommand(unittest.TestCase):
 
@@ -12,8 +12,8 @@ class TestGhSemverNoSubcommand(unittest.TestCase):
         print("Setting up TestGhSemverNoSubcommand testbed")
         cls.cli_path = os.path.abspath('gh-semver.py')
         cls.test_dir = os.path.abspath('./testbed/TestGhSemverNoSubcommand')
-        cls.__cleanup_testbed()
-        cls.__create_testbed()
+        Testbed.cleanup_testbed(cls.test_dir)
+        Testbed.create_testbed(cls.test_dir)
 
 
     @classmethod
@@ -21,21 +21,6 @@ class TestGhSemverNoSubcommand(unittest.TestCase):
         # Class-level teardown code
         print("Tearing down TestGhSemverCLI class")
         #cls.__cleanup_testbed()
-
-    @classmethod
-    def __cleanup_testbed(cls):
-        if os.path.exists(cls.test_dir):
-            print("Cleaning up the testbed")
-            try:
-                subprocess.check_call('rm -rf {}'.format(cls.test_dir), shell=True)
-            except subprocess.CalledProcessError as e:
-                print(f"Failed to remove test directory: {e}")
-                
-        
-    @classmethod
-    def __create_testbed(cls):
-        subprocess.check_call('mkdir -p {}'.format(cls.test_dir), shell=True)
-        subprocess.check_call('git init', cwd=cls.test_dir, shell=True)
 
 
     def run_cli(self, *args, cwd=None):
@@ -49,9 +34,9 @@ class TestGhSemverNoSubcommand(unittest.TestCase):
     @pytest.mark.smoke
     def test_no_subcommand_initial(self):
         """This test checks if the script returns the default initial version when no config files are setup - it requires a clean testbed"""
-        self.__cleanup_testbed()
-        self.__create_testbed()
-        result = self.run_cli()
+        Testbed.cleanup_testbed(self.test_dir)
+        Testbed.create_testbed(self.test_dir)
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir)
         self.assertIn(
             "v0.0.0", result.stdout)
         
@@ -59,25 +44,25 @@ class TestGhSemverNoSubcommand(unittest.TestCase):
     @pytest.mark.smoke
     def test_no_subcommand_from_config(self):
         """This test checks if the config files are read correctly, if they exist - it requires a clean testbed"""
-        self.__cleanup_testbed()
-        self.__create_testbed() 
+        Testbed.cleanup_testbed(self.test_dir)
+        Testbed.create_testbed(self.test_dir)
         # Create a .semver.config file with a variant differnt from the default prefix and initial offset       
         subprocess.check_call('echo "[semver]\\n  prefix = ver\\n  initial = 1.0.0">.semver.config', cwd=self.test_dir, shell=True)
-        result = self.run_cli()    
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir)    
         self.assertIn(
             "ver1.0.0", result.stdout)
         
         # Even if cwd is not the root of the git repo, the script should still read the .semver.config file
         subprocess.check_call('mkdir not-root', cwd=self.test_dir, shell=True)
         subprocess.check_call('cd not-root', cwd=self.test_dir, shell=True)
-        result = self.run_cli(cwd=self.test_dir+"/not-root")    
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir+"/not-root")
         self.assertIn(
             "ver1.0.0", result.stdout)
        
         # Set the initial and prefix values in the git config, they should override the values in the .semver.config file
         subprocess.check_call('git config set --local semver.initial 2.0.0', cwd=self.test_dir, shell=True)
         subprocess.check_call('git config set --local semver.prefix version', cwd=self.test_dir, shell=True)
-        result = self.run_cli()    
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir)     
         self.assertIn(
             "version2.0.0", result.stdout)
         
@@ -89,21 +74,21 @@ class TestGhSemverNoSubcommand(unittest.TestCase):
         subprocess.check_call('git add -A', cwd=self.test_dir, shell=True)
         subprocess.check_call('git commit -m "added testfile"', cwd=self.test_dir, shell=True)
         subprocess.check_call('git tag -a -m zerozeroone v0.0.1', cwd=self.test_dir, shell=True)
-        result = self.run_cli()
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir) 
         self.assertIn("v0.0.1", result.stdout)
 
         subprocess.check_call('git tag -a -m onetwoone ver1.2.1', cwd=self.test_dir, shell=True)
-        result = self.run_cli()
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir) 
         self.assertIn("ver1.2.1", result.stdout)
 
         subprocess.check_call('git tag -a -m oneoneone version1.1.1', cwd=self.test_dir, shell=True)
-        result = self.run_cli()
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir) 
         self.assertIn("ver1.2.1", result.stdout)
 
         subprocess.check_call('git tag -a -m twooneone version2.1.1-freetext', cwd=self.test_dir, shell=True)
-        result = self.run_cli()
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir) 
         self.assertIn("version2.1.1-freetext", result.stdout)
 
         subprocess.check_call('git tag -a -m nonvalid version3.11-freetext', cwd=self.test_dir, shell=True)
-        result = self.run_cli()
+        result = Testbed.run_cli(self.cli_path, cwd=self.test_dir) 
         self.assertIn("version2.1.1-freetext", result.stdout)
---
+++ tests/testbed.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+
+class Testbed:
+    @staticmethod
+    def cleanup_testbed(test_dir):
+        if os.path.exists(test_dir):
+            print("Cleaning up the testbed")
+            try:
+                subprocess.check_call('rm -rf {}'.format(test_dir), shell=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Failed to remove test directory: {e}")
+                
+        
+    @staticmethod
+    def create_testbed(test_dir):
+        subprocess.check_call('mkdir -p {}'.format(test_dir), shell=True)
+        subprocess.check_call('git init', cwd=test_dir, shell=True)
+
+    @staticmethod
+    def run_cli(cli_path, *args, cwd):
+        result = subprocess.run(
+            ['python3', cli_path] + list(args), cwd=cwd, capture_output=True, text=True)
+        return result
