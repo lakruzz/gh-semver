ab09960a753ddad269b675a2ce5509bf50f966ca
Lars Kruse <lakr@thetechcollective.eu>
2025-06-05 13:15:07 +0000

    added verbose support - #34

--- classes/semver.py
+++ classes/semver.py
@@ -13,40 +13,52 @@ class Semver:
     """Class used to represent the semver state of git repository"""
 
     # Class constants
-    config_file = ".semver.config"    
+    config_file_name = ".semver.config"
+    _verbose = False
+    props = {
+        'config_file': None,
+        'prefix': None,
+        'initial': None,
+        'suffix': None,
+        'workdir': None,
+        'git_root': None,
+        'config': None,
+        'semver_tags': None,
+        'current_semver': None,
+        'next': None,
+    }
 
     # Class variables
 
-    prefix = None
-    initial = None
-    suffix = None
-    workdir = os.getcwd() # Required, therefore also  set at class level to allow a null constructor
-    git_root = None
-    config = {}
-    semver_tags = {}
-    current_semver = None
-    current_tag = None
-    next ={}
 
-    def __run_git(self, cmd=str):
+    def __run_git(self, cmd=str, die_on_error=True) -> list[str, subprocess.CompletedProcess]:
+        self.verbose_print(f"⚙️  {cmd}")
         result = subprocess.run(
-        cmd, capture_output=True, text=True, shell=True, cwd=self.workdir)
-        return result    
+        cmd, capture_output=True, text=True, shell=True, cwd=self.props['workdir'])
+        if result.returncode != 0 and die_on_error:
+            print(f"Error: {result.stderr.strip()}", file=sys.stderr)
+            sys.exit(1)
+        return [result.stdout.strip(), result]    
           
     # Instance methods
     def __init__(self, workdir=None):
         if workdir:
             # check if the directory exists
             if not os.path.exists(workdir):
-                raise FileNotFoundError(f"Directory {workdir} does not exist")
-            self.workdir = os.path.abspath(workdir)
+                print (f"Error: Directory {workdir} does not exist", file=sys.stderr)
+                sys.exit(1)
+            self.set('workdir', os.path.abspath(workdir))
+        else:
+            self.set('workdir', os.getcwd())
 
-        result = self.__run_git('git rev-parse --show-toplevel')
-        if not result.returncode == 0: 
-            raise FileNotFoundError(f"{result.stderr}")
+        [git_root, result] = self.__run_git('git rev-parse --show-toplevel', die_on_error=False)
+        if result.returncode != 0:
+            print(f"fatal: not a git repository (or any of the parent directories): .git\n", file=sys.stderr)
             sys.exit(1)
-        self.git_root = result.stdout.strip()
-        self.config_file = self.git_root + '/' + self.config_file
+ 
+        self.set('git_root', git_root)    
+        
+        self.set('config_file', self.props['git_root'] + '/' + self.config_file_name)
 
         self.__read_semver_tags()
         self.__read_semver_config()
@@ -63,93 +75,94 @@ class Semver:
         Also sets the next major, minor and patch versions in the next dictionary.
         """
 
-        tags = self.__run_git('git tag').stdout.strip().split('\n')
+        [tags,_] = self.__run_git('git tag')
+        tags = tags.split('\n')
         semver_pattern = re.compile(r'(\d+)\.(\d+)\.(\d+)')
 
-        self.semver_tags = {}
+        semver_tags = {}
 
         for tag in tags:
             match = semver_pattern.search(tag)
             if match:
-                self.semver_tags[tuple(map(int, match.groups()))] = tag
+                semver_tags[tuple(map(int, match.groups()))] = tag
 
-        if self.semver_tags:
-            sorted_semver_tags = sorted(self.semver_tags.keys())
-            self.current_semver = sorted_semver_tags[-1]
-            self.current_tag = self.semver_tags[sorted_semver_tags[-1]]
+        self.set('semver_tags', semver_tags)
 
-        if not self.current_semver:
+        if semver_tags:
+            sorted_semver_tags = sorted(semver_tags.keys())
+            self.set('current_semver', sorted_semver_tags[-1])
+            self.set('current_tag', semver_tags[sorted_semver_tags[-1]])
+
+        if not self.props['current_semver']:
             self.__set_current_semver_from_initial()
 
         self.__set_next_versions()
 
     def __set_current_semver_from_initial(self):
 
-        if self.initial == None:
-            self.initial = '0.0.0'
+        if self.props['initial'] == None:
+            self.props['initial'] = '0.0.0'
         try:
-            self.current_semver = tuple(map(int, self.initial.split('.')))
+            self.props['current_semver'] = tuple(map(int, self.props['initial'].split('.')))
         except Exception as e:
             raise ValueError(f"Failed to parse initial version, doesn't look like a three-level integer: {e}")
         
         suffix = ''
-        if self.suffix and self.suffix != '':
-            suffix = f"{self.suffix}"
+        if self.props['suffix'] is not None:
+            suffix = self.props['suffix']
             
-        self.current_tag = f"{self.prefix}{self.initial}{suffix}"
+        self.set('current_tag', f"{self.props['prefix']}{self.props['initial']}{suffix}")
     
 
     def __set_next_versions(self):    
         # Bump major, reset minor and patch
-        self.next['major'] = f"{self.current_semver[0] + 1}.0.0"
+        next = {}
+        next['major'] = f"{self.props['current_semver'][0] + 1}.0.0"
         # Leave major, bump minor, reset patch
-        self.next['minor'] = f"{self.current_semver[0]}.{self.current_semver[1] + 1}.0"
+        next['minor'] = f"{self.props['current_semver'][0]}.{self.props['current_semver'][1] + 1}.0"
         # Leave major and minor, bump patch
-        self.next['patch'] = f"{self.current_semver[0]}.{self.current_semver[1]}.{self.current_semver[2] + 1}"
+        next['patch'] = f"{self.props['current_semver'][0]}.{self.props['current_semver'][1]}.{self.props['current_semver'][2] + 1}"
+
+        self.set('next', next)
+
+    
      
     def __read_semver_config(self):
         """Read the .semver.config file and store the configuration as a dictionary"""
         
-        self.config = {}
-
+        config = {}
         # The config_file may not exist - we don't care. Just continue and return an empty dict
-        config = self.__run_git(f'git config list --file {self.config_file}').stdout
+        [output,_] = self.__run_git(f"git config list --file {self.props['config_file']}", die_on_error=False)
+
         try:
-            for line in config.split('\n'):
-                key, value = line.split('=')
-                if key == 'semver.prefix':
-                    self.config['prefix'] = value
-                elif key == 'semver.initial':
-                    self.config['initial'] = value
-                elif key == 'semver.suffix':
-                    self.config['suffix'] = value
+            config = {line.split('=')[0]: line.split('=')[1] for line in output.split('\n') if '=' in line}
         except Exception:
             pass
 
-        self.prefix = self.config.get('prefix', '')
-        self.initial = self.config.get('initial', '0.0.0')
-        self.suffix = self.config.get('suffix', '')
-        if self.semver_tags.keys().__len__() == 0:
+        self.set('config', config)
+
+        self.set('prefix', config.get('semver.prefix', ''))
+        self.set('initial', config.get('semver.initial', '0.0.0'))
+        self.set('suffix', config.get('semver.suffix', ''))
+        if self.props['semver_tags'].keys().__len__() == 0:
             self.__set_current_semver_from_initial()
             self.__set_next_versions()
     
     def set_config(self, prefix=None, initial=None, suffix=None):
         """Set the configuration in the .semver.config file"""
 
-        updated = False
         args = {'prefix': prefix, 'initial': initial, 'suffix': suffix}
         for setting in args.keys():
             if args[setting]:
-                result = self.__run_git(f'git config --file {self.config_file} semver.{setting} {args[setting]}')
-                updated = True     
+                [_,_] = self.__run_git(f'git config --file {self.props['config_file']} semver.{setting} {args[setting]}')
         self.__read_semver_config()
         
   
     def bump(self, level=str, message=None, suffix=None):
         cmd = self.get_git_tag_cmd(level, message, suffix)
-        result = self.__run_git(cmd)
+        [_,_] = self.__run_git(cmd)
         self.__read_semver_tags()
-        return self.current_tag
+        return self.props['current_tag']
 
     def get_git_tag_cmd(self, level=str, message=None, suffix=None):
         if message:
@@ -160,11 +173,25 @@ class Semver:
         if suffix:
             suffix = f"{suffix}"
         else:
-            if self.suffix: 
-                suffix = f"{self.suffix}"
+            if self.props['suffix']:
+                suffix = f"{self.props['suffix']}"
             else:
                 suffix = ""
-        
-        next_tag = f"{self.prefix}{self.next[level]}{suffix}"
 
-        return f"git tag -a -m \"{next_tag}\nBumped {level} from version '{self.current_tag}' to '{next_tag}'{message}\" {next_tag}"
+        next_tag = f"{self.props['prefix']}{self.props['next'][level]}{suffix}"
+
+        return f"git tag -a -m \"{next_tag}\nBumped {level} from version '{self.props['current_tag']}' to '{next_tag}'{message}\" {next_tag}"
+
+    def verbose_print(self, message:str):
+        """Prints the message if verbose is enabled"""
+        if self._verbose:
+            print(message)
+
+    def set(self, prop:str, value):
+        self.props[prop] = value
+        if self._verbose:
+            print(f"🏷️  '{prop}': {self.props[prop]}")
+    
+    @classmethod
+    def verbose(cls, verbose:bool = True):
+        cls._verbose = verbose
